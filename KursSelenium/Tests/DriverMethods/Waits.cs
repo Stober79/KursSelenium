95c5461dc821961a333c2402c2e8452c356d9092
Robert Steiman <robertsteiman@op.pl>
2021-10-26 13:32:31 +0200

    Waits and others

---
+++ KursSelenium/Tests/DriverMethods/Waits.cs
@@ -0,0 +1,108 @@
+﻿using KursSelenium.Element;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace KursSelenium.Tests.DriverMethods
+{
+    class Waits
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            driver.Navigate().GoToUrl(Url.FakestoreMainPage());
+            Button.InfoList(driver).Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        }
+        [Test]
+
+        public void Test()
+        {
+            driver.Navigate().GoToUrl("https://fakestore.testelka.pl/product/grecja-limnos/");
+            driver.FindElement(By.CssSelector("[name='add-to-cart'")).Click();
+            driver.FindElement(By.CssSelector(".woocommerce-message .wc-forward")).Click();
+            IWebElement quantityField = wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementIsVisible(By.CssSelector("[id^='quantity']")));
+            //IWebElement quantityField = wait.Until(driver=>driver.FindElement(By.CssSelector("[id^='quantity']")));
+            quantityField.Clear();
+            quantityField.SendKeys("2");
+            driver.FindElement(By.CssSelector("[name='update_cart']")).Click();
+
+            wait.Until(driver => driver.FindElements(By.CssSelector(".blockUI")).Count == 0);
+
+            string amount = driver.FindElement(By.CssSelector(".order-total .amount")).Text;
+            string expected = "6 400,00 zł";
+            Assert.AreEqual(expected, amount, "Total price is not correct.");
+            
+        }
+        [Test]
+
+        public void GetLocalizationAndTagName()
+        {
+            IWebElement windsurfingImage = wait.Until(driver => driver.FindElement(By.CssSelector("[alt='Windsurfing']")));
+            Point location = windsurfingImage.Location;
+            Size size = windsurfingImage.Size;
+            string tagName = windsurfingImage.TagName;
+        }
+
+        [Test]
+
+        public void DisplayedCouponFieldTest()
+        {
+            driver.Navigate().GoToUrl("https://fakestore.testelka.pl/product/grecja-limnos/");
+            driver.FindElement(By.CssSelector("[name='add-to-cart'")).Click();
+            driver.FindElement(By.CssSelector(".woocommerce-message .wc-forward")).Click();
+            wait.Until(driver => driver.FindElement((By.CssSelector(".checkout-button")))).Click();
+            bool isCouponFieldDisplayed = wait.Until(driver => driver.FindElement(By.CssSelector("input#coupon_code"))).Displayed;
+            Assert.IsFalse(isCouponFieldDisplayed);
+        }
+        [Test]
+        public void SelecteRememberMeTest()
+        {
+            driver.Navigate().GoToUrl(Url.FakestoreMyAccount());
+            Checkbox.RememberMe(driver).Click();
+            bool isSelected = wait.Until(driver => Checkbox.RememberMe(driver)).Selected;
+
+            Assert.IsTrue(isSelected);
+        }
+        [Test]
+        public void GetAtribiuteTest()
+        {
+            IWebElement search = Field.Search(driver);
+            string placeholder = search.GetAttribute("placeholder");
+            Assert.AreEqual("Szukaj produktów…", placeholder, "");
+        }
+        [Test]
+        public void GetCSSStylesTest()
+        {
+            IWebElement search = Field.Search(driver);
+            string color = search.GetCssValue("background-color");
+            Assert.AreEqual("rgba(242, 242, 242, 1)", color, "");
+        }
+        [Test]
+        public void GetPropertyTest()
+        {
+            driver.Navigate().GoToUrl(Url.FakestoreFuerteventuraSotavento());
+            IWebElement amountOfProduct = driver.FindElement(By.CssSelector("input.qty"));
+            string maxAmount = amountOfProduct.GetProperty("max");
+            Assert.AreEqual("11844", maxAmount, "");
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+
+    }
+}
--- KursSelenium/Tests/XPath/XPathZadania.cs
+++ KursSelenium/Tests/XPath/XPathZadania.cs
@@ -14,29 +14,34 @@ namespace KursSelenium.Tests.XPath
     class XPathZadania
     {
         IWebDriver driver;
+        WebDriverWait wait;
         
         [SetUp]
         public void Setup()
         {
             driver = new ChromeDriver();
-            Start.Setup(driver);
+            driver.Navigate().GoToUrl(Url.FakestoreMainPage());
+            driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            Button.InfoList(driver).Click();
         }
 
         [Test]
         public void Test()
         {
-            driver.Navigate().GoToUrl(Url.FakestoreMainPage());
-            Button.InfoList(driver).Click();
+            wait = new WebDriverWait(driver,TimeSpan.FromSeconds(5));
             driver.FindElement(By.XPath(".//*[contains(@class,'recent-product')]//a[@data-product_id='4116']")).Click();
             driver.FindElement(By.XPath(".//*[contains(@class,'recent-product')]//a[@data-product_id='4114']")).Click();
             driver.FindElement(By.XPath(".//*[contains(@class,'recent-product')]//a[@data-product_id='393']")).Click();
+            wait.Until(driver=>driver.FindElement(By.CssSelector("li.post-393>a[class='added_to_cart wc-forward']")));
             driver.FindElement(By.CssSelector("li.post-393>a[class='added_to_cart wc-forward']")).Click();
             IWebElement pole = driver.FindElement(By.XPath(".//td[@class='product-remove']//a[@data-product_id='393']/../following-sibling::td[@class='product-quantity']//input"));
             pole.Clear();
-            pole.SendKeys("2"); 
-            string kwota = "9 599,99 zł";
-            string compare = driver.FindElement(By.XPath(".//tr[@class='cart-subtotal']//span")).Text;
-            Assert.AreEqual(kwota, compare, "Different values");
+            pole.SendKeys("2");
+            driver.FindElement(By.CssSelector("[name='update_cart']")).Click();
+            string expected = "9 599,99 zł";
+            string amount = driver.FindElement(By.XPath(".//tr[@class='cart-subtotal']//span")).Text;
+            Assert.AreEqual(expected, amount, "Different values");
         }
 
         [TearDown]
