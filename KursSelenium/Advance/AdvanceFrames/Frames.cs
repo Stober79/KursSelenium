4917e9d3b57404684ff72ae8a052326d37c18788
Robert Steiman <robertsteiman@op.pl>
2021-10-28 11:40:11 +0200

    Ramki

---
+++ KursSelenium/Advance/AdvanceFrames/Frames.cs
@@ -0,0 +1,50 @@
+﻿using KursSelenium.Element;
+using KursSelenium.StartSetup;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursSelenium.Advance.AdvenceFrames
+{
+    class Frames
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Start.Setup(driver);
+            driver.Navigate().GoToUrl("https://fakestore.testelka.pl/actions-przyklady/");
+            Button.InfoList(driver);
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void FrameExampleTest()
+        {
+            driver.SwitchTo().Frame(0);// tu jest użyty numer ramki ale można użyć nazwy ramki( atrybut id lub name) lub id WebElementu
+            driver.FindElement(By.XPath(".//li[text()='Item 5']"));
+            driver.SwitchTo().DefaultContent(); //wychodzi do głównego dokumentu 
+            //driver.SwitchTo().ParentFrame();do poprzedającej ramki jeśli nie było żadnej poprzedzającej to do główego dokumentu
+        }
+        [Test]
+        public void FrameExampleTest1()
+        {
+            driver.SwitchTo().Frame("select");
+            driver.FindElement(By.XPath(".//li[text()='Item 5']"));
+        }
+        [Test]
+        public void FrameExampleTest2()
+        {
+            driver.SwitchTo().Frame(driver.FindElement(By.CssSelector("#select")));
+            driver.FindElement(By.XPath(".//li[text()='Item 5']"));
+        }
+    }
+}
---
+++ KursSelenium/Advance/AdvanceFrames/FramesZadanie.cs
@@ -0,0 +1,71 @@
+﻿using KursSelenium.Element;
+using KursSelenium.StartSetup;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursSelenium.Advance.AdvanceFrames
+{
+    class FramesZadanie
+    {
+        IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Start.Setup(driver);
+            driver.Navigate().GoToUrl("https://fakestore.testelka.pl/cwiczenia-z-ramek/");
+            Button.InfoList(driver).Click();
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void Test1()
+        {
+            driver.SwitchTo().Frame("main-frame");
+            bool isButtonEnabled = driver.FindElement(By.CssSelector("input[name='main-page']")).Enabled;
+            Assert.IsFalse(isButtonEnabled, "Button is enabled");
+            
+        }
+        [Test]
+        public void Test2()
+        {
+            driver.SwitchTo().Frame("main-frame");
+            driver.SwitchTo().Frame("image");
+            string tekst = driver.FindElement(By.XPath(".//img[@alt='Wakacje']/..")).GetAttribute("href");
+            string expected = "https://fakestore.testelka.pl/";
+            Assert.AreEqual(expected, tekst,"Niepoprawny link");
+
+        }
+        [Test]
+        public void Test3()
+        {
+            driver.SwitchTo().Frame("main-frame");
+            driver.SwitchTo().Frame("image");
+            driver.SwitchTo().Frame(0);
+            bool isButtonEnabled = driver.FindElement(By.CssSelector("p>a.button")).Enabled;
+            Assert.IsTrue(isButtonEnabled, "Button is disabled");
+
+        }
+        [Test]
+        public void Test4()
+        {
+            driver.SwitchTo().Frame("main-frame");
+            driver.SwitchTo().Frame("image");
+            driver.SwitchTo().Frame(0);
+            driver.FindElement(By.CssSelector("p>a.button")).Click();
+            driver.SwitchTo().ParentFrame().SwitchTo().ParentFrame();
+            driver.FindElement(By.CssSelector("a[name='climbing']")).Click();
+            bool isDisplayd = driver.FindElement(By.CssSelector("image.custom-logo")).Displayed;
+            Assert.IsTrue(isDisplayd, "Image is not displayed");
+
+        }
+    }
+}
