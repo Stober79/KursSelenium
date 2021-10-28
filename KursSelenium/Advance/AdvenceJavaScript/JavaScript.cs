85599338852d4272ec949726e65fd142c8245469
Robert Steiman <robertsteiman@op.pl>
2021-10-28 12:49:03 +0200

    JavaScript

---
+++ KursSelenium/Advance/AdvenceJavaScript/JavaScript.cs
@@ -0,0 +1,52 @@
+﻿using KursSelenium.Element;
+using KursSelenium.StartSetup;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace KursSelenium.Advance.AdvenceJavaScript
+{
+    class JavaScript
+    {
+        IWebDriver driver;
+        IJavaScriptExecutor jse;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Start.Setup(driver);
+            driver.Navigate().GoToUrl(Url.FakestoreFuerteventuraSotavento());
+            Button.InfoList(driver).Click();
+            jse = (IJavaScriptExecutor)driver;
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void ClicButtonTest()
+        {
+            jse.ExecuteScript("console.log('test')");//console.log(;jfgjkd') to Jscript wyświtlający w konsoli podany tekst
+            string tekst = (string) jse.ExecuteScript("return 'To tylko tekst'");//zwaraca tekst potrzebne rzutowanie
+            IWebElement adToCartBUtton = driver.FindElement(By.CssSelector("[name='add-to-cart']"));
+            jse.ExecuteScript("arguments[0].click()", adToCartBUtton);//arguments[0] to podajemy w ktory argument podany w liście ma kliknąć
+
+        }
+        [Test]
+        public void AsyncScriptTest()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            jse.ExecuteAsyncScript("window.setTimeout(arguments[arguments.length - 1], 500);");
+            stopwatch.Stop();
+            Console.WriteLine("Elapsed time: " + stopwatch.ElapsedMilliseconds);
+
+        }
+    }
+}
---
+++ KursSelenium/Advance/AdvenceJavaScript/JavaScriptZadanie.cs
@@ -0,0 +1,42 @@
+﻿using KursSelenium.Element;
+using KursSelenium.StartSetup;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace KursSelenium.Advance.AdvenceJavaScript
+{
+    class JavaScriptZadanie
+    {
+        IWebDriver driver;
+        IJavaScriptExecutor jse;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Start.Setup(driver);
+            driver.Navigate().GoToUrl(Url.FakestoreFuerteventuraSotavento());
+            Button.InfoList(driver).Click();
+            jse = (IJavaScriptExecutor)driver;
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void Test()
+        {
+            IWebElement opis = driver.FindElement(By.CssSelector("#tab-description"));
+            jse.ExecuteScript("arguments[0].scrollIntoView(true)", opis);
+            IWebElement stickyAddToCart = driver.FindElement(By.CssSelector(".storefront-sticky-add-to-cart"));
+            Assert.IsTrue(stickyAddToCart.GetAttribute("class").Contains("storefront-sticky-add-to-cart--slideInDown"), "List is not visble.");
+        }
+    }
+
+}
