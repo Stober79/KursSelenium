6a5d901a8ef7aeca72b1e0c08f0becd630b14dd4
Robert Steiman <robertsteiman@op.pl>
2021-10-29 09:02:30 +0200

    First Class

---
+++ KursSelenium/Tests/Advance/AdvenceAlerts/Alerts.cs
@@ -0,0 +1,37 @@
+﻿using KursSelenium.Element;
+using KursSelenium.StartSetup;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KursSelenium.Tests.Advance.AdvenceAlerts
+{
+    class Alerts
+    {
+        IWebDriver driver;
+        IJavaScriptExecutor jse;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Start.Setup(driver);
+            driver.Navigate().GoToUrl(Url.FakestoreFuerteventuraSotavento());
+            Button.InfoList(driver).Click();
+            jse = (IJavaScriptExecutor)driver;
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void Test()
+        {
+
+        }
+    }
+}
