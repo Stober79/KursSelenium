5faee237ef9fcad259cabf0b71a141251e3bae38
Robert Steiman <robertsteiman@op.pl>
2021-10-29 13:21:57 +0200

    small changes

--- KursSelenium/Tests/Advance/AdvenceAlerts/AlertZadanie.cs
+++ KursSelenium/Tests/Advance/AdvenceAlerts/AlertZadanie.cs
@@ -13,6 +13,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
     {
         IWebDriver driver;
         IJavaScriptExecutor jse;
+        string text;
 
         [SetUp]
         public void Setup()
@@ -33,7 +34,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
         {
             driver.FindElement(By.CssSelector("button[onclick='confirmFunction()']")).Click();
             driver.SwitchTo().Alert().Accept();
-            string text = driver.FindElement(By.CssSelector("p#demo")).Text;
+            text = driver.FindElement(By.CssSelector("p#demo")).Text;
             Assert.AreEqual("Wybrana opcja to OK!", text, "Nie wybrano OK");
         }
         [Test]
@@ -41,7 +42,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
         {
             driver.FindElement(By.CssSelector("button[onclick='confirmFunction()']")).Click();  
             driver.SwitchTo().Alert().Dismiss();
-            string text = driver.FindElement(By.CssSelector("p#demo")).Text;
+            text = driver.FindElement(By.CssSelector("p#demo")).Text;
             Assert.AreEqual("Wybrana opcja to Cancel!", text, "Nie wybrano OK");
         }
         [Test]
@@ -50,7 +51,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
             driver.FindElement(By.CssSelector("button[onclick='promptFunction()']")).Click();
             driver.SwitchTo().Alert().SendKeys("Robert Steiman");
             driver.SwitchTo().Alert().Accept();
-            string text = driver.FindElement(By.CssSelector("p#prompt-demo")).Text;
+            text = driver.FindElement(By.CssSelector("p#prompt-demo")).Text;
             Assert.AreEqual("Cześć Robert Steiman! Jak leci?", text, "Nie wybrano OK");
         }
         [Test]
@@ -58,7 +59,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
         {
             driver.FindElement(By.CssSelector("button[onclick='promptFunction()']")).Click();
             driver.SwitchTo().Alert().Accept();
-            string text = driver.FindElement(By.CssSelector("p#prompt-demo")).Text;
+            text = driver.FindElement(By.CssSelector("p#prompt-demo")).Text;
             Assert.AreEqual("Cześć Harry Potter! Jak leci?", text, "Nie wybrano OK");
         }
         [Test]
@@ -66,7 +67,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
         {
             driver.FindElement(By.CssSelector("button[onclick='promptFunction()']")).Click();
             driver.SwitchTo().Alert().Dismiss();
-            string text = driver.FindElement(By.CssSelector("p#prompt-demo")).Text;
+            text = driver.FindElement(By.CssSelector("p#prompt-demo")).Text;
             Assert.AreEqual("Użytkownik anulował akcję.", text, "Nie wybrano OK");
         }
         [Test]
@@ -79,7 +80,7 @@ namespace KursSelenium.Tests.Advance.AdvenceAlerts
                     try { return driver.SwitchTo().Alert(); }
                     catch  (NoAlertPresentException) { return null; }
                 });
-            string text = driver.SwitchTo().Alert().Text;
+            text = driver.SwitchTo().Alert().Text;
             driver.SwitchTo().Alert().Accept();
             Assert.AreEqual("Miałem mały poślizg", text, "Nie wybrano OK");
         }
