5d82ba5d6aaded494b758eca33a14631301f9542
Robert Steiman <robertsteiman@op.pl>
2021-10-29 12:14:38 +0200

    Cookies zadanie

---
+++ KursSelenium/Tests/Advance/AdvenceCookies/CookieZadanie.cs
@@ -0,0 +1,51 @@
+﻿using KursSelenium.Element;
+using KursSelenium.StartSetup;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace KursSelenium.Tests.Advance.AdvenceCookies
+{
+    class CookieZadanie
+    {
+        IWebDriver driver;
+        WebDriverWait wait;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            Start.Setup(driver);
+            driver.Navigate().GoToUrl("https://fakestore.testelka.pl/product/wspinaczka-island-peak/");
+            Button.InfoList(driver).Click();
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+        }
+        [TearDown]
+        public void Quit()
+        {
+            driver.Quit();
+        }
+        [Test]
+        public void Test1()
+        {
+            driver.FindElement(By.CssSelector("button[name='add-to-cart']")).Click();
+            string cookieValue = driver.Manage().Cookies.GetCookieNamed("woocommerce_recently_viewed").Value;
+            Assert.AreEqual("42", cookieValue, "Cookie Value is not correct.");
+        }
+        [Test]
+        public void Test2()
+        {
+            driver.FindElement(By.CssSelector("a.add_to_wishlist ")).Click();
+            //wait.Until(ExpectedConditions.ElementIsVisible(By.CssSelector("div#yith-wcwl-message"))); ten był mojej konstukcji :)))))
+            wait.Until(driver => driver.Manage().Cookies.AllCookies.Count==4);
+            IList<Cookie> cookieList = driver.Manage().Cookies.AllCookies;
+            string cookieValue = cookieList.Where(cookie => cookie.Name.StartsWith("yith_wcwl_session")).Select(cookie => cookie.Value).First();
+            Assert.IsNotEmpty( cookieValue, "Cookie Value is  empty.");
+        }
+    }
+}
--- KursSelenium/Tests/Advance/AdvenceCookies/Cookies.cs
+++ KursSelenium/Tests/Advance/AdvenceCookies/Cookies.cs
@@ -3,13 +3,15 @@ using KursSelenium.StartSetup;
 using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace KursSelenium.Tests.Advance.AdvenceCookies
 {
     class Cookies
     {
         IWebDriver driver;
-        IJavaScriptExecutor jse;
 
         [SetUp]
         public void Setup()
@@ -18,7 +20,6 @@ namespace KursSelenium.Tests.Advance.AdvenceCookies
             Start.Setup(driver);
             driver.Navigate().GoToUrl(Url.FakestoreFuerteventuraSotavento());
             Button.InfoList(driver).Click();
-            jse = (IJavaScriptExecutor)driver;
         }
         [TearDown]
         public void Quit()
@@ -26,8 +27,67 @@ namespace KursSelenium.Tests.Advance.AdvenceCookies
             driver.Quit();
         }
         [Test]
-        public void Test()
-        { 
+        public void GetCookies()
+        {
+            IList<Cookie> cookies = driver.Manage().Cookies.AllCookies;
+            Assert.AreEqual(1, cookies.Count, "");
+        }
+        [Test]
+        public void GetCookie()
+        {
+            string cookieValue = driver.Manage().Cookies.GetCookieNamed("store_noticef7db772cd2958546f5ffc7e2822d64e8").Value;
+            Assert.IsNotEmpty(cookieValue, "store_noticef7db772cd2958546f5ffc7e2822d64e8 is empty");
+
+        }
+        [Test]
+        public void AddCookie()
+        {
+            Cookie newcookie = new Cookie("new cookie", "hello!", "fakestore.testelka.pl", "/", DateTime.Now + TimeSpan.FromSeconds(30));
+            driver.Manage().Cookies.AddCookie(newcookie);
+            int numberOfCookies = driver.Manage().Cookies.AllCookies.Count;
+            Assert.AreEqual(2, numberOfCookies, "NUmber of cookies is not what expected");
+
+        }
+        [Test]
+        public void DeleteCookieNamed()
+        {
+           
+            driver.Manage().Cookies.DeleteCookieNamed("store_noticef7db772cd2958546f5ffc7e2822d64e8");
+            int numberOfCookies = driver.Manage().Cookies.AllCookies.Count;
+            Assert.AreEqual(2, numberOfCookies, "NUmber of cookies is not what expected");
+
+        }
+        [Test]
+        public void DeleteCookie()
+        {
+            Cookie newcookie = new Cookie("new cookie", "hello!", "fakestore.testelka.pl", "/", DateTime.Now + TimeSpan.FromSeconds(30));
+            driver.Manage().Cookies.AddCookie(newcookie);
+            driver.Manage().Cookies.DeleteCookie(newcookie);
+            int numberOfCookies = driver.Manage().Cookies.AllCookies.Count;
+            Assert.AreEqual(1, numberOfCookies, "NUmber of cookies is not what expected");
+
+        }
+        [Test]
+        public void GetStoreNoticeCookie()
+        {
+
+            IList<Cookie> cookies = driver.Manage().Cookies.AllCookies;
+            Cookie storeNoticeCookie = null;
+            foreach (Cookie cookie in cookies)
+            {
+                if (cookie.Name.StartsWith("store_notice"))
+                    storeNoticeCookie = cookie;
+            }
+            Assert.AreEqual("hidden", storeNoticeCookie.Value, "Store notice cookie value is not what expected.");
+
+        }
+        [Test]
+        public void GetStoreNoticeCookie2()
+        {
+
+            IList<Cookie> cookies = driver.Manage().Cookies.AllCookies;
+            string cookieValue = cookies.Where(cookie => cookie.Name.StartsWith("store_notice")).Select(cookie => cookie.Value).First();
+            Assert.AreEqual("hidden", cookieValue, "Store notice cookie value is not what expected.");
 
         }
     }
